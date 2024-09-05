73cb4beb6ba153c4a58fdfdaf5fc607aabe54afc
Nikolai Petukhov <n.petukhov@innopolis.university>
2024-09-05 20:26:26 +0300

    reading from file is upd

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -2,34 +2,60 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <utility>
 
 #include "token.hpp"
 #include "lexer.hpp"
+
+using namespace std;
 
 Lexer::Lexer(const std::string& file_name)
 {
     m_source_file.open(file_name, std::ios::in);
 }
 
-std::string Lexer::get_word()
+pair<string, char> Lexer::get_word()
 {
-    return "for";
+    char ch;
+    pair<string, char> word;
+
+    while (true) {
+        m_source_file.get(ch);
+        if (ch == '.') {
+            // ...
+        }
+        // TODO: handle each punctuation syntax
+
+        if (ch == ' ') {
+            word.second = ' ';
+            return word;
+        }
+
+        if (ch == '\n') {
+            word.second = '\n';
+            return word;
+        }
+
+        word.first += ch;
+    }
 }
 
 std::vector<Token> Lexer::parse()
 {
     std::vector<Token> res;
 
-    std::string tok;
+    pair<string, char> tok;
     while (m_source_file.eof()) {
-        m_source_file >> tok;
+        tok = get_word();
 
-        int word_id = tokens.at(tok);
+        int word_id = tokens.at(tok.first);
         switch (word_id)
             {
                 case 1:
-                    res.emplace_back(tok, word_id);
+                    res.emplace_back(tok.first, word_id);
             }
+
+
     }
     
     return res;
--- src/lexer/lexer.hpp
+++ src/lexer/lexer.hpp
@@ -9,11 +9,11 @@
 class Lexer
 {
 public:
-    std::ifstream m_source_file;
+    std::fstream m_source_file;
     std::vector<int> m_tokens;
 
     explicit Lexer(const std::string& file_name);
-    
-    std::string get_word();
+
+    std::pair<std::string, char> get_word();
     std::vector<Token> parse();
 };
